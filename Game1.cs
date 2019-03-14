7c42b42755b95f75c7a012b7a97b9137c9587fbb
pawo98 <piwosz49@o2.pl>
2019-03-14 11:24:12 +0100

    Zadanie1

---
+++ Game1.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Game1
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        private Sprite sprite1;
+        private Sprite sprite2;
+        SpriteFont text;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            var texture = Content.Load<Texture2D>("mario");
+            sprite1 = new Sprite(texture)
+            {
+                speed = 3f,
+                position = new Vector2(100, 100)
+            };
+            sprite2 = new Sprite(texture)
+            {
+                speed = 0f,
+                position = new Vector2(300, 300)
+            };
+            text = Content.Load<SpriteFont>("Collision");
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            sprite1.Update();
+            sprite2.Update();
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            spriteBatch.Begin();
+
+            sprite1.Draw(spriteBatch);
+            sprite2.Draw(spriteBatch);
+            if (sprite1.checkCollision(sprite2)) spriteBatch.DrawString(text, "Zly dotyk", new Vector2(200, 0), Color.White);
+
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Sprite.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game1
+{
+    class Sprite
+    {
+        public Texture2D texture;
+        public Vector2 position;
+        public float speed = 0f;
+
+        public Sprite(Texture2D texture)
+        {
+            this.texture = texture;
+        }
+
+        public void Update()
+        {
+            if (speed <= 0) return;
+            if (Keyboard.GetState().IsKeyDown(Keys.Up))
+            {
+                position.Y -= speed;
+            }
+            else if (Keyboard.GetState().IsKeyDown(Keys.Down))
+            {
+                position.Y += speed;
+            }
+            else if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                position.X -= speed;
+            }
+            else if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                position.X += speed;
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture,position,Color.White);
+        }
+
+
+        //Check Collision
+        public bool checkLeftSide(Sprite sprite)
+        {
+            return this.position.X < sprite.position.X + sprite.texture.Width &&
+                this.position.Y < sprite.position.Y + sprite.texture.Height &&
+                this.position.Y + texture.Height > sprite.position.Y;
+        }
+
+        public bool checkRightSide(Sprite sprite)
+        {
+            return this.position.X + texture.Width > sprite.position.X &&
+                this.position.Y < sprite.position.Y + sprite.texture.Height &&
+                this.position.Y + texture.Height > sprite.position.Y;
+        }
+
+        public bool checkTopSide(Sprite sprite)
+        {
+            return this.position.Y < sprite.position.Y + sprite.texture.Height &&
+                this.position.X < sprite.position.X + sprite.texture.Width &&
+                this.position.X + texture.Width > sprite.position.X;
+        }
+
+        public bool checkBottomSide(Sprite sprite)
+        {
+            return this.position.Y + texture.Height > sprite.position.Y &&
+                this.position.X < sprite.position.X + sprite.texture.Width &&
+                this.position.X + texture.Width > sprite.position.X;
+        }
+
+        public bool checkCollision(Sprite sprite)
+        {
+            return (checkBottomSide(sprite) && checkLeftSide(sprite) && checkRightSide(sprite) && checkTopSide(sprite));
+        }
+    }
+}
